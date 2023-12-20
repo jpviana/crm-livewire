64a0bae1101e6018a1a0f92ace78bee303a1645b
joao Paulo <joaopaulosis@hotmail.com>
2023-12-20 16:02:14 -0300

    wip

--- config/database.php
+++ config/database.php
@@ -129,8 +129,8 @@ return [
         ],
 
         'default' => [
-            'url'=> env('REDIS_URL'),
-            'host'     => env('REDIS_HOST', '127.0.0.1'),
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port'     => env('REDIS_PORT', '6379'),
