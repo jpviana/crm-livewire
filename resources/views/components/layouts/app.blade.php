db8cb31d84970487b84328391fba1bb7a6e89136
joao Paulo <joaopaulosis@hotmail.com>
2023-12-20 17:25:36 -0300

    crm-1: wip

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $title ?? 'Page Title' }}</title>
     </head>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <body>
         {{ $slot }}
